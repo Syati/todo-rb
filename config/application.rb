36253796bc6c1a929610439c5dacf35a495ac680
mizuki-y <mizuki-y@syati.info>
2017-09-18 11:05:45 +0900

    Fix application html slim with partial header foooter

--- config/application.rb
+++ config/application.rb
@@ -9,17 +9,18 @@ Bundler.require(*Rails.groups)
 module Todos
   class Application < Rails::Application
     config.load_defaults 5.1
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.helper false
       g.assets false
       g.template_engine = :slim
       g.test_framework :rspec,
                        fixtures: true,
+                       request_specs: true,
                        view_specs: true,
                        helper_specs: false,
                        routing_specs: false,
-                       controller_specs: true,
-                       request_specs: false
+                       controller_specs: false
     end
   end
 end
