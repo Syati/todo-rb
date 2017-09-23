be42e7daff587dbd721200bbb0d5ad434fb63b65
mizuki-y <mizuki-y@syati.info>
2017-09-23 17:03:18 +0900

    Add User model and gem device

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails', '~> 5.1.4'
+gem 'devise'
 gem 'slim-rails'
 gem 'mysql2', '>= 0.3.18', '< 0.5'
 gem 'puma', '~> 3.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  devise_for :users
   root              to: 'static_pages#home'
   get    'about',   to: 'static_pages#about'
   get    'company', to: 'static_pages#company'
