4498df5b437be53c7eef7cd7e979bd17c5d04335
mizuki-y <mizuki-y@syati.info>
2017-09-24 14:49:18 +0900

    Use webpacker instead of sprocket

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,10 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+// css
+import "normalize.css"
+import "milligram"
+
+import '../src/application';
+
+
