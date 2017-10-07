60cb8456b543123adea6b99f4e2371c2b6b6cea9
mizuki-y <mizuki-y@syati.info>
2017-10-07 17:51:37 +0900

    Override milligram var to use my color

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root 'todos#index', as: :authenticated_root
+  end
+
   root              to: 'static_pages#home'
   get    'about',   to: 'static_pages#about'
   get    'company', to: 'static_pages#company'
