99fad9a0578e18def6a2280e0743c586b758374c
mizuki-y <mizuki-y@syati.info>
2018-01-01 01:57:15 +0900

    Put a link on todo title instead of edit btn

--- spec/views/todos/index.html.slim_spec.rb
+++ spec/views/todos/index.html.slim_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe 'todos/index.html.slim', type: :view do
   it 'render index' do
     todo = create(:todo)
     assign(:todos, [todo])
+
     render
+
     assert_select 'h1', text: 'Todos'
     assert_select 'tbody>tr', 1
-    assert_select 'td', text: todo.title
+    assert_select 'td a[href=?]', todo_path(todo), text: todo.title
     assert_select 'td', text: todo.status
-    assert_select 'td>a', href: 'hoge'
   end
 end
